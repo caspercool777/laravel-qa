63242148808e6b4c0d315f799b91ccb7d9e314cd
Your Name <you@example.com>
2024-11-19 12:27:23 +0100

    Extend the form-update

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -7,17 +6,17 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Ask Question</h2>
+                        <h2>Edit Question</h2>
                         <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                         </div>
                     </div>
 
                 </div>
-
                 <div class="card-body">
-                   <form action="{{ route('questions.store') }}" method="post">
-                    @include ("questions._form", ['buttonText' => "Ask Question"])
+                   <form action="{{ route('questions.update', $question->id) }}" method="post">
+                        {{ method_field('PUT') }}
+                        @include ("questions._form", ['buttonText' => "Update Question"])
                    </form>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::get('/questions', [QuestionsController::class, 'index'])->name('questions
 Route::get('/questions/create', [QuestionsController::class, 'create'])->name('questions.create');
 Route::post('/questions/store', [QuestionsController::class, 'store'])->name('questions.store');
 Route::get('/questions/{question}', [QuestionsController::class, 'show'])->name('questions.show');
+Route::get('/questions/edit/{question}', [QuestionsController::class, 'edit'])->name('questions.edit');
+Route::put('/questions/update/{question}', [QuestionsController::class, 'update'])->name('questions.update');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
